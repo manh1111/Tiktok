4d2b946f962d4dc5a38758df455d01b7b02bdf6b
manh1111 <minhanh746347@gmail.com>
2023-10-03 01:24:07 +0700

    2/10

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Fragment } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from './routes';
 import { DefaultLayout } from '~/components/Layout';
 
@@ -9,25 +9,30 @@ function App() {
       <div className="App">
         <Routes>
           {publicRoutes.map((route, index) => {
-            let Layout = DefaultLayout
+            let Layout = DefaultLayout;
 
             if (route.layout) {
-              Layout = route.layout
+              Layout = route.layout;
             } else if (route.layout === null) {
-              Layout = Fragment
+              Layout = Fragment;
             }
-            const Page = route.component
-            return <Route key={index} path={route.path} element={
-              <Layout>
-                <Page />
-              </Layout>} />;
+            const Page = route.component;
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
           })}
         </Routes>
       </div>
     </Router>
   );
-
-
 }
 
 export default App;
